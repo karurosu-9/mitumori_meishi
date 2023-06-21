2d3e7ed93d80d6df7675a28e11f7da7f7fe78c14
carlos <cl0xtc43x.apxma@gmail.com>
2023-06-22 00:21:16 +0900

    showのビューとshowアクションの作成

--- meishi_mitumori/app/Http/Controllers/CorpController.php
+++ meishi_mitumori/app/Http/Controllers/CorpController.php
@@ -17,4 +17,16 @@ class CorpController extends Controller
 
         return view('corp.index', $data);
     }
+
+    public function show(Corp $corp)
+    {
+        $postalCode = substr($corp->postal_code, 0, 3) . '-' . substr($corp->postal_code, 3);
+
+        $data = [
+            'corp' => $corp,
+            'postalCode' => $postalCode,
+        ];
+
+        return view('corp.show', $data);
+    }
 }
---
+++ meishi_mitumori/resources/views/corp/show.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.layout')
+
+@section('title', '会社-詳細')
+
+@section('content')
+
+@endsection
+<br>
+<br>
+<br>
+<br>
+<h1>会社-詳細</h1>
+<br>
+<br>
+<table cellpadding="1">
+    <tr>
+        <th>会社名</th>
+        <td>{{ $corp->corp_name }}</td>
+    </tr>
+    <tr>
+        <th>郵便番号</th>
+        <td>{{ $postalCode }}</td>
+    </tr>
+    <tr>
+        <th>住所</th>
+        <td>{{ $corp->address }}</td>
+    </tr>
+    <tr>
+        <th>電話番号</th>
+        <td>{{ $corp->tel }}</td>
+    </tr>
+</table>
+<br>
+<br>
+<button onclick="location.href='{{ route('corp.list') }}'">戻る</button>
