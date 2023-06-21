8bf3ec5d46a9295004eb6f89acd5063874b8a11b
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-06-21 14:29:49 +0900

    Corpsのマイグレーションファイルとseederファイルの作成

---
+++ meishi_mitumori/database/seeders/CorpsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CorpsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('corps')->insert([
+            'corp_name' => 'アスカ株式会社',
+            'postal_code' => '1234567',
+            'address' => '東京都新宿区',
+            'tel' => '1234567890',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
