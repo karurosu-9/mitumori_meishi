09768b985581cc3d6991b10f395cb396510b6bf6
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-06-21 14:30:20 +0900

    Corpモデルの作成、定義

---
+++ meishi_mitumori/app/Models/Corp.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Corp extends Model
+{
+    use HasFactory;
+
+    protected $table = 'corps';
+    protected $fillable = ['corp_name', 'postal_code', 'address', 'tel'];
+
+    //タイムスタンプの自動更新を有効
+    public $timestamps = true;
+}
