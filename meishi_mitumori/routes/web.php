c883df3133e911390b197fd1eb26d564a9bfd7f0
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-06-21 21:09:42 +0900

    corpのindex、showルートの設定

--- meishi_mitumori/routes/web.php
+++ meishi_mitumori/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CorpController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('corp', [CorpController::class, 'index'])->name('corp.list');
+Route::get('corp/show/{corp}', [CorpController::class, 'show'])->name('corp.show');
