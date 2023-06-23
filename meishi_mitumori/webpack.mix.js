7b4e9ce35463b5f2a852bb33277efaa54f03f8bb
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-06-23 19:11:54 +0900

    書き方変更

--- meishi_mitumori/webpack.mix.js
+++ meishi_mitumori/webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/corp.js', 'public/js/corp.js')
+mix.js('resources/js/corp.js', 'public/js')
     .css('resources/css/pages/layout.css', 'public/css/pages/layout.css')
 
