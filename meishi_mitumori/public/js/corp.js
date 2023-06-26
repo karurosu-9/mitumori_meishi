a5b2495512506c439901aa53fd95cc9d7924d97e
carlos <cl0xtc43x.apxma@gmail.com>
2023-06-27 00:32:08 +0900

    business_cards_listのテーブルの表示をデータが無かった時の表示方法を追加

--- meishi_mitumori/public/js/corp.js
+++ meishi_mitumori/public/js/corp.js
@@ -21,8 +21,8 @@ function editCorp(url) {
 window.editCorp = editCorp;
 function deleteCorp(event, url) {
   //会社名の値の取得
-  var corpName = event.target.getAttribute('data-corp-name');
-  if (confirm('会社名『 ' + corpName + ' 』を本当に削除してよろしいですか？')) {
+  var corpName = event.target.getAttribute("data-corp-name");
+  if (confirm("会社名『 " + corpName + " 』を本当に削除してよろしいですか？")) {
     location.href = url;
   }
 }
@@ -32,72 +32,77 @@ window.deleteCorp = deleteCorp;
 function addCheckMessage() {
   //未記入の項目があった場合、その項目名を入れる変数
   var emptyFields = [];
-  var corpNameElement = document.getElementById('corp_name');
-  var corpName = corpNameElement ? corpNameElement.value : '';
-  if (corpName === '') {
-    emptyFields.push('会社名');
+  var corpNameElement = document.getElementById("corp_name");
+  var corpName = corpNameElement ? corpNameElement.value : "";
+  if (corpName === "") {
+    emptyFields.push("会社名");
   }
-  var postalCodeElement = document.getElementById('postal_code');
-  var postalCode = postalCodeElement ? postalCodeElement.value : '';
-  if (postalCode === '') {
-    emptyFields.push('郵便番号');
+  var postalCodeElement = document.getElementById("postal_code");
+  var postalCode = postalCodeElement ? postalCodeElement.value : "";
+  if (postalCode === "") {
+    emptyFields.push("郵便番号");
   }
-  var addressElement = document.getElementById('address');
-  var address = addressElement ? addressElement.value : '';
-  if (address === '') {
-    emptyFields.push('住所');
+  var addressElement = document.getElementById("address");
+  var address = addressElement ? addressElement.value : "";
+  if (address === "") {
+    emptyFields.push("住所");
   }
-  var telElement = document.getElementById('tel');
-  var tel = telElement ? telElement.value : '';
-  if (tel === '') {
-    emptyFields.push('電話番号');
+  var telElement = document.getElementById("tel");
+  var tel = telElement ? telElement.value : "";
+  if (tel === "") {
+    emptyFields.push("電話番号");
   }
 
   //未記入の項目のあった時の処理
   if (emptyFields.length > 0) {
-    var emptyFieldsString = emptyFields.join(',');
-    alert('未記入の欄があります。:' + emptyFieldsString);
+    var emptyFieldsString = emptyFields.join(",");
+    alert("未記入の欄があります。:" + emptyFieldsString);
     return false;
   }
-  var msg = confirm('この内容で登録してもよろしいですか？');
+  var msg = confirm("この内容で登録してもよろしいですか？");
   return msg;
 }
 window.addCheckMessage = addCheckMessage;
 
 //business_cards_list用のselect-box、table表示のjsコード
-var selectElement = document.getElementById('select-division');
+var selectElement = document.getElementById("select-division");
 
 //セレクトボックスの値が変更された時に呼び出される関数
-selectElement.addEventListener('change', function () {
+selectElement.addEventListener("change", function () {
   var selectedDivision = this.value;
 
   //部署ごとに表示する名刺データの処理をする関数
   displayBusinessCards(selectedDivision);
 });
 function displayBusinessCards(selectedDivision) {
-  //全ての<tr>の要素を取得
   var tableRows = document.querySelectorAll('#business-cards-table tr');
+  var noDataMessage = document.querySelector('.no-data-message');
+  var dataRowCount = 0; // データ行の数をカウントする変数
 
-  //最初の<tr>行は<th>の値になるだけなので、取得しない。それ以降の<tr>行をデータがあるだけ取得
   for (var i = 1; i < tableRows.length; i++) {
-    /*変数rowには
-        <tr>
-            <td>～</td>から
-            <td>～</td>までを1行とする値を行ごとにrowに格納
-        </tr>
-    */
     var row = tableRows[i];
-    //1行の3番目の<td>の値({{ $card->division }})を取得
     var division = row.cells[2].textContent;
     if (selectedDivision === '') {
       row.style.display = 'table-row';
+      dataRowCount++;
     } else if (division === selectedDivision) {
       row.style.display = 'table-row';
+      dataRowCount++;
     } else {
       row.style.display = 'none';
     }
   }
+  if (noDataMessage) {
+    if (dataRowCount === 0) {
+      noDataMessage.style.display = 'block'; // 表示する
+    } else {
+      noDataMessage.style.display = 'none'; // 非表示にする
+    }
+  }
 }
+
+// 初期表示時に実行する
+displayBusinessCards('');
 window.displayBusinessCards = displayBusinessCards;
 
 /***/ }),
