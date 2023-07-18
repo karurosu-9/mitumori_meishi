0507d01c13699e71da0dffd73277a8b91d9c2e5d
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-07-18 21:31:40 +0900

    モデル名の修正

--- meishi_mitumori/app/Http/Controllers/DivisionController.php
+++ meishi_mitumori/app/Http/Controllers/DivisionController.php
@@ -3,7 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Division;
+use App\Models\Corp;
 
 class DivisionController extends Controller
 {
+    public function add(Corp $corp)
+    {
+        $corps = Corp::all();
+        $data = [
+            'corps' => $corps,
+            'corp' => $corp,
+        ];
+
+        return view('division.add', $data);
+    }
 }
