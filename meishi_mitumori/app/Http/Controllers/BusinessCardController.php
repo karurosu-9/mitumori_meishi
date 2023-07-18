8d060d2a126f8c68abcd84db65b26706ce76a17a
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-07-18 10:20:30 +0900

    Corpモデルを使用できるようにuse文の追加

--- meishi_mitumori/app/Http/Controllers/BusinessCardController.php
+++ meishi_mitumori/app/Http/Controllers/BusinessCardController.php
@@ -5,14 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\BusinessCard;
 use App\Https\Requests\CreateBusinessCardRequest;
+use App\Models\Corp;
 
 class BusinessCardController extends Controller
 {
     public function add()
     {
         $corps = Corp::all();
-
-
 
         $data = [
             'corps' => $corps,
@@ -23,6 +22,5 @@ class BusinessCardController extends Controller
 
     public function create()
     {
-
     }
 }
