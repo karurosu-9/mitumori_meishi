4aaf8bb07c947fb2043f8a104f13068dc2c42038
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-07-19 13:23:56 +0900

    division.jsファイルの削除

--- meishi_mitumori/webpack.mix.js
+++ meishi_mitumori/webpack.mix.js
@@ -2,6 +2,5 @@ const mix = require('laravel-mix');
 
 mix.browserSync('localhost:8000');
 mix.js('resources/js/corp.js', 'public/js')
-    .js('resources/js/division.js', 'public/js')
     .css('resources/css/pages/layout.css', 'public/css/pages/layout.css');
 
