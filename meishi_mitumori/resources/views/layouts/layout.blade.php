b63613d8e0d1b8c0b5fade4208e1d8ef1647e3f2
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-07-19 13:14:27 +0900

    インデント修正

--- meishi_mitumori/resources/views/layouts/layout.blade.php
+++ meishi_mitumori/resources/views/layouts/layout.blade.php
@@ -1,22 +1,27 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>@yield('title')</title>
     <!-- ここに共通のCSSやJavaScriptのリンクなどを追加 -->
     <link href="{{ asset('css/pages/layout.css') }}" rel="stylesheet">
-    
 </head>
+
 <body>
     <header>
         <!-- トップページへのリンクなどを追加 -->
-        <div><h1>title</h1></div>
+        <div>
+            <h1>title</h1>
+        </div>
     </header>
 
     <main>
-        @yield('content') <!-- 各ページの個別のコンテンツがここに挿入される -->
+        @yield('content')
+        <!-- 各ページの個別のコンテンツがここに挿入される -->
     </main>
 
     <footer>
     </footer>
 </body>
+
 </html>
