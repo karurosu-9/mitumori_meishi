1cb4c786c4d08884eac63e19ebf613be72737d22
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-07-25 19:06:50 +0900

    見積の入力フォームの作成
    addビューでの見積入力フォームの作成

---
+++ meishi_mitumori/resources/views/estimate/add.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.layout')
+
+@section('title', '見積り-登録')
+
+@section('content')
+    <br>
+    <br>
+    <br>
+    <h1>【 {{ $corp->corp_name }} の見積り-登録 】</h1>
+    <br>
+    <br>
+    <form action="{{ route('estimate.add', ['corp' => $corp]) }}" method="POST" onsubmit="return formCheckMessage()">
+        @csrf
+        @include('forms._estimate_form', [
+            'backUrl' => route('corp.estimateList', ['corp' => $corp]),
+            'buttonLabelValue' => '見積り一覧へ戻る',
+        ])
+    </form>
+    <br>
+    <br>
+
+    <script src="{{ asset('js/estimate.js') }}"></script>
+@endsection
