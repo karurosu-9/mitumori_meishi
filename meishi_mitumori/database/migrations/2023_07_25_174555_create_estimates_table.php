6581a3e120acf20b4de95bfcfaf9aa3ac7e6fb74
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-07-25 19:02:12 +0900

    マイグレーションファイルの作成
    Estimatesテーブルのマイグレーションファイルの作成

---
+++ meishi_mitumori/database/migrations/2023_07_25_174555_create_estimates_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use App\Consts\FormCountConsts;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estimates', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('corp_id');
+
+            $table->string('tekiyo1', 100)->nullable(false);
+            $table->string('unit_price1', 50)->nullable(false);
+            $table->string('quantity1', 50)->nullable(false);
+            $table->string('amount1', 50)->nullable(false);
+            $table->string('note1', 255)->nullable();
+
+            //マジックナンバーを使用
+            for ($i = 2; $i <= FormCountConsts::FORM_NOT_HOSOKU; $i++) {
+                $table->string('tekiyo' . $i, 100)->nullable();
+                $table->string('unit_price' . $i, 50)->nullable();
+                $table->string('quantity' . $i, 50)->nullable();
+                $table->string('amount' . $i, 50)->nullable();
+                $table->string('note' . $i, 255)->nullable();
+            }
+
+            //マジックナンバーを使用
+            for ($i = 1; $i <= FormCountConsts::FORM_HOSOKU; $i++) {
+                $table->string('hosoku' . $i, 255)->nullable();
+            }
+
+            $table->unsignedBigInteger('total_amount')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estimates');
+    }
+};
