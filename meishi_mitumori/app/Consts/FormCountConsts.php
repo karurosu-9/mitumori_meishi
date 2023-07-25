6fffcd447f576c94626ccd5390e41e5ec7619cd7
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-07-25 19:04:06 +0900

    マジックナンバーの設定
    見積の入力フォームで使用するマジックナンバーの作成　エイリアスの設定

---
+++ meishi_mitumori/app/Consts/FormCountConsts.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Consts;
+
+//viesやmigrationファイルで使用するマジックナンバー
+class FormCountConsts
+{
+    //見積り入力フォームなどで使用する、補足以外の行数(摘要、単価など)
+    public const FORM_NOT_HOSOKU = 5;
+
+    //見積入力フォームなどでしようする、補足の行数
+    public const FORM_HOSOKU = 3;
+}
