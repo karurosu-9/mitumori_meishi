e9f39554f8df4a86bf2642d96884477bfe95321b
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-07-25 19:05:11 +0900

    Estimateコントローラーの作成
    Estimateコントローラーとaddアクションの枠の作成

---
+++ meishi_mitumori/app/Http/Controllers/EstimateController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Estimate;
+
+class EstimateController extends Controller
+{
+    public function add(Corp $corp)
+    {
+    }
+}
