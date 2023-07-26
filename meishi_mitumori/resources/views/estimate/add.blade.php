6875ab2f3bf5b2a88f5c0102bfaba60690f72d83
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-07-26 10:39:04 +0900

    estimate.addビューの'backUrl'遷移先の変更
    corp.estimateListからestimate.corpEstimateListへ変更

--- meishi_mitumori/resources/views/estimate/add.blade.php
+++ meishi_mitumori/resources/views/estimate/add.blade.php
@@ -12,7 +12,7 @@
     <form action="{{ route('estimate.add', ['corp' => $corp]) }}" method="POST" onsubmit="return formCheckMessage()">
         @csrf
         @include('forms._estimate_form', [
-            'backUrl' => route('corp.estimateList', ['corp' => $corp]),
+            'backUrl' => route('estimate.corpEstimateList', ['corp' => $corp]),
             'buttonLabelValue' => '見積り一覧へ戻る',
         ])
     </form>
