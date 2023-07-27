31fd216f35b5f145dfd4d4c15a8fb46db4286f66
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-07-27 12:04:08 +0900

    MyCorpモデルの作成

---
+++ meishi_mitumori/app/Models/MyCorp.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MyCorp extends Model
+{
+    use HasFactory;
+
+    protected $table = 'my_table';
+    protected $fillable = ['corp_name', 'postal_code', 'address', 'tel', 'fax', 'place', 'conditions', 'deadline'];
+
+    public $timestamps = true;
+}
