4bc5cd47a59f7ecbe2bba2217ff0de87ed6dd21b
Takafumi Uchiyama <uchiyama@asca.co.jp>
2023-07-28 16:23:53 +0900

    estimateモデルに$fillableの設定

--- meishi_mitumori/app/Models/Estimate.php
+++ meishi_mitumori/app/Models/Estimate.php
@@ -4,15 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Consts\EstimateFormCountConsts;
 
 class Estimate extends Model
 {
     use HasFactory;
 
     protected $table = 'estimates';
-    protected $fillable = [];
-
+    protected $fillable = ['date', 'corp_id', 'total_price'];
     public $timestamps = true;
+
+    public function __construct(array $attributes = [])
+    {
+        for ($i = 1; $i <= EstimateFormCountConsts::FORM_NOT_HOSOKU; $i++) {
+            $this->fillable[] = 'tekiyo' . $i;
+            $this->fillable[] = 'unit_price' . $i;
+            $this->fillable[] = 'quantity' . $i;
+            $this->fillable[] = 'amount' . $i;
+            $this->fillable[] = 'note' . $i;
+        }
+
+        parent::__construct($attributes);
+    }
 
     public function corp()
     {
